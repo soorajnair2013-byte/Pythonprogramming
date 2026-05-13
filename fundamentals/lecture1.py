8e1cab055d521bf63a52d93e3a5cff41008965a4
soorajnair2013-byte <sooraj.nair2013@gmail.com>
2026-05-13 19:49:31 +0530

    first lecture

---
+++ fundamentals/lecture1.py
@@ -0,0 +1,29 @@
+#single line comment
+
+"""
+multi line comment
+"""
+
+'''
+1.i will ask user to enter a number
+2.two numbers will be assigned to two variables
+3.one variable: and=var1+var2
+4. i will print the number
+
+execution:
+1. input() function is used to take input from user
+2. assign two variables with input()
+ variable1=int(input("enter first number:"))
+3. assign variable2=int(input("enter second number:"))  
+4. print  the output
+
+'''
+
+variable1=int(input("enter first number:"))
+variable2=int(input("enter second number:"))
+ans=variable1+variable2
+print("the sum of two numbers is:",ans) 
+
+
+
+
