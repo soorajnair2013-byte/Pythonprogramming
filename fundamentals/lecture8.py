7b78f7351c9521bf062538648e8d3b6285072de8
soorajnair2013-byte <sooraj.nair2013@gmail.com>
2026-05-22 16:57:18 +0530

    'done'

---
+++ fundamentals/lecture8.py
@@ -0,0 +1,31 @@
+t=(2,4)
+a=t
+print(a*2)
+
+
+c= set((x for x in range (0,10)))  #the values are distict in nature
+c
+
+
+c.pop()
+c
+
+c.add(0)
+c
+
+f={1,1,2,3,4,5,5}
+
+f.add(0) #indexing cannot be used with sets
+f
+
+
+a=list({i for i in range(0,10)})  # typecasting set to list
+a
+
+data=[100,100,200,300,100,500,100]
+ndata=set(data)
+ndata
+
+df=tuple(ndata)
+df[3]
+
