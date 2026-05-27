a0c47aca95bc28fa66e3818fe02267fde8ccda5b
soorajnair2013-byte <sooraj.nair2013@gmail.com>
2026-05-27 16:54:45 +0530

    'done'

---
+++ fundamentals/lecture11.py
@@ -0,0 +1,36 @@
+sum =100
+
+i=10
+
+if i <=10:
+  i= sum + i
+  pass
+  if i <=3:
+    print("hii")
+
+    if i<=2:
+      print('world')
+
+print(sum)
+
+
+
+def sum(num1=100,num2=200):
+  num3=num1+num2
+  print(num3)
+
+sum()  
+
+a=10
+def sum(num3,num1=100,num2=200):
+  num4=num1+num2+num3
+  print(num4)
+sum(1)  
+
+
+
+a=10
+def sum(num3,num1=100,num2=200):
+  num4=num1+num2+num3
+  print(num4)
+sum(1,1,1)  
