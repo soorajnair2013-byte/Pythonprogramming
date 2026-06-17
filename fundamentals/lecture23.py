1da9fabddd25893e0d6a308109f79b91f9d6fa6e
soorajnair2013-byte <sooraj.nair2013@gmail.com>
2026-06-17 16:52:37 +0530

    'done'

---
+++ fundamentals/lecture23.py
@@ -0,0 +1,76 @@
+# higher the varience higher the bias 
+# sample size more than 30 z test / 3 or -3 should be the value
+# less than 30 t test
+
+# chisquare test
+
+# 5% of outliers are allowed but data should be normally distributed
+
+
+# covarience--dont have a numerical value
+# typres--+ve,-ve or 0 
+
+# limitation
+# magnitude depends on units
+
+
+# co-relation- r^2-->1<x<-1 will have numbers
+
+# corealtin=covarience/std deviation
+
+
+# measures of association
+# covarience
+# corelation
+
+'''
+
+1.in order to evaluate correlation ensure all the columns  are in one scale
+ 3 types of scaling technique
+ standard scaler
+ minmax scalr
+ robust scaler
+
+
+ note: 
+ a.when the dataset in normally distributed (symmetric data) then go for standard scaler
+ b.a.when the dataset in non normal distributed (skewed data) then go for min max scaler
+ c.a.when the dataset is non normal distributed (skewed data) also containng lot
+  of outliers ie. outliers makes machine learning model sesitive then othen go for
+ robust scaler
+
+when q1=q3 drop the column
+
+3 types of skewness
+positive
+negative
+zero skewness
+
+ 3 type of kurtosis:
+leptokurtic 
+misokurtic
+platykurtic
+
+
+
+
+
+ '''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
